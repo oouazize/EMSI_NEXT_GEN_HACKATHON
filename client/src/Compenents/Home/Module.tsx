56df7b4adc29facf38606f00ac0dd0be48feadb0
oouazize <oouazize@student.1337.ma>
2024-06-16 04:22:55 +0100

    chore: Update Dockerfile and SCSS files

--- client/src/Compenents/Home/Module.tsx
+++ client/src/Compenents/Home/Module.tsx
@@ -1,41 +1,43 @@
-import { useState } from 'react'
-import { useNavigate } from "react-router-dom"
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
-export default function Module({Little, Desc, level, id}) {
-  const isCenterElement = id === 1 || (10 - id) % 3 === 0;
-  const navigate = useNavigate()
+export default function Module({ Little, Desc, level, id }) {
+	const isCenterElement = id === 1 || (10 - id) % 3 === 0;
+	const navigate = useNavigate();
 
-  const handleRedirect = () => {
-    navigate("/ModuleView")
-  }
+	const handleRedirect = () => {
+		navigate("/ModuleView");
+	};
 
-  const primaryColor = "#FFD910"
-  const diamondColor = "#2E2E2E"
-  const secondaryColor = "#F5F5F5"
+	const primaryColor = "#FFD910";
+	const diamondColor = "#2E2E2E";
+	const secondaryColor = "#F5F5F5";
 
-  return (
-    <div
-      className={`Module Module-${id}`}
-      onClick={handleRedirect}
-      style={{backgroundColor: isCenterElement ? primaryColor : diamondColor}}>
+	return (
+		<div
+			className={`Module`}
+			onClick={handleRedirect}
+			style={{ backgroundColor: isCenterElement ? primaryColor : diamondColor }}
+		>
+			<span style={{ color: isCenterElement ? diamondColor : primaryColor }}>
+				{Little}
+			</span>
 
-        <span style={{color: isCenterElement ? diamondColor : primaryColor}}>
-          {Little}
-        </span>
+			<p style={{ color: isCenterElement ? "#13157D" : "#DFDFDF" }}>{Desc}</p>
 
-        <p style={{color: isCenterElement ? "#13157D" : "#DFDFDF"}}>
-          {Desc}
-        </p>
-
-        <div className="item level" style={{backgroundColor: secondaryColor}}>
-          <div className="level-bar-fill"
-            style={{
-              backgroundColor: isCenterElement ? diamondColor : primaryColor,
-              color: isCenterElement ? secondaryColor : diamondColor,
-              width: `${level}%`
-            }}></div>
-          <p style={{color: isCenterElement ? secondaryColor : diamondColor}}>{level}%</p>
-        </div>
-    </div>
-  )
+			<div className="item level" style={{ backgroundColor: secondaryColor }}>
+				<div
+					className="level-bar-fill"
+					style={{
+						backgroundColor: isCenterElement ? diamondColor : primaryColor,
+						color: isCenterElement ? secondaryColor : diamondColor,
+						width: `${level}%`,
+					}}
+				></div>
+				<p style={{ color: isCenterElement ? secondaryColor : diamondColor }}>
+					{level}%
+				</p>
+			</div>
+		</div>
+	);
 }
